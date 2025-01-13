eb7a4bef996448b5610382f6a813f7ca6f5dcbd7
WataruOhkura <wohkura@nexseed.net>
2025-01-13 08:49:29 +0800

    fixed design

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 "use strict";
 
 {
-    window.addEventListener("scroll", function () {
-        // スクロール量を取得
-        const scroll = window.scrollY;
+  window.addEventListener("scroll", function () {
+    // スクロール量を取得
+    const scroll = window.scrollY;
 
-        //画面の高さを取得
-        const windowHeight = window.innerHeight;
+    //画面の高さを取得
+    const windowHeight = window.innerHeight;
 
-        //全ての.boxを取得
-        const boxes = document.querySelectorAll(".box");
+    //全ての.boxを取得
+    const boxes = document.querySelectorAll(".box");
 
-        boxes.forEach((box) => {
-            // boxの高さを取得
-            const distanceToBox = box.offsetTop;
+    boxes.forEach((box) => {
+      // boxの高さを取得
+      const distanceToBox = box.offsetTop;
 
-            // 下記条件が成り立つ時だけboxにis-activeクラスを付与する
-            if (scroll + windowHeight > distanceToBox + 100) {
-                box.classList.add("is-active");
-            }
-        });
+      // 下記条件が成り立つ時だけboxにis-activeクラスを付与する
+      if (scroll + windowHeight > distanceToBox + 100) {
+        box.classList.add("is-active");
+      }
     });
+  });
 }
